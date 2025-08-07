ce97291c0b1ac692bff97b9892f80db08662c612
no-int <tkdduf2122@naver.com>
2025-08-08 01:07:32 +0900

    init

---
+++ src/test/java/com/trevari/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trevari.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
