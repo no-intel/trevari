6c5ff2f6ac6585db919c35e928a8703ab4dbcab6
no-int <tkdduf2122@naver.com>
2025-08-10 20:53:24 +0900

    [#9] BookRepository fix

--- src/main/java/com/trevari/test/domain/book/repository/BookRepository.java
+++ src/main/java/com/trevari/test/domain/book/repository/BookRepository.java
@@ -3,5 +3,9 @@ package com.trevari.test.domain.book.repository;
 import com.trevari.test.domain.book.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Integer> {
+import java.util.List;
+import java.util.Optional;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findBookByIsbn(Long isbn);
 }
