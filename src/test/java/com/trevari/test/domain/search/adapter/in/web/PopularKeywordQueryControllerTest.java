9bb9d808a889892db742e55e065f4dc18ad5093e
no-int <tkdduf2122@naver.com>
2025-08-13 08:15:00 +0900

    [#14] 인기 검색어 조회 API

---
+++ src/test/java/com/trevari/test/domain/search/adapter/in/web/PopularKeywordQueryControllerTest.java
@@ -0,0 +1,51 @@
+package com.trevari.test.domain.search.adapter.in.web;
+
+import com.trevari.test.domain.search.applicateion.PopularKeywordQueryService;
+import com.trevari.test.domain.search.port.out.redis.KeywordRankResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(PopularKeywordQueryController.class)
+class PopularKeywordQueryControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockitoBean
+    PopularKeywordQueryService service;
+
+    @Test
+    @DisplayName("인기 검색어 Top10 조회 API")
+    void getTopKeywords_returnsOkResponse() throws Exception {
+        // given
+        List<KeywordRankResponse> mockList = List.of(
+                new KeywordRankResponse(1, "apple", 5L),
+                new KeywordRankResponse(2, "banana", 3L)
+        );
+
+        when(service.getTopKeywords()).thenReturn(mockList);
+
+        // when & then
+        mockMvc.perform(get("/api/search/popular-keywords")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data[0].rank").value(1))
+                .andExpect(jsonPath("$.data[0].keyword").value("apple"))
+                .andExpect(jsonPath("$.data[0].score").value(5))
+                .andExpect(jsonPath("$.data[1].rank").value(2))
+                .andExpect(jsonPath("$.data[1].keyword").value("banana"))
+                .andExpect(jsonPath("$.data[1].score").value(3));
+    }
+}
